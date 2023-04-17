72b58421933488726abf93b28f23de1d7950b967
emollarf <elena.mollar@hotmail.com>
2023-04-17 16:28:42 +0200

    let-const-var

---
+++ 02-Fundamentos/const-var-let.js
@@ -0,0 +1,30 @@
+
+var nombre = 'hola';
+
+
+if(true){
+    var nombre = 'magneto';
+}
+
+console.log(nombre);
+
+
+let nombre1 = 'hola';
+
+if(true){
+    let nombre1 = 'magneto';
+    nombre1='Adios';
+    console.log(nombre1);
+}
+
+console.log(nombre);
+
+const nombre2 = 'hola';
+
+if(true){
+    const nombre2 = 'magneto';
+    nombre2='Adios';
+    console.log(nombre1);
+}
+
+console.log(nombre);
