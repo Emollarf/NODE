e4a57a5b2e2512ec7f9719e96ef66b2e9324267f
emollarf <elena.mollar@hotmail.com>
2023-04-24 17:15:04 +0200

    Bases de node

---
+++ 03-Bases-Node/app.js
@@ -0,0 +1,24 @@
+
+
+//imprimir la tabla del 5 en consola hasta el 10
+
+const fs = require('fs');
+
+console.clear();
+console.log('=================')
+console.log('TABLA DEL 5')
+console.log('=================')
+
+const base = 3;
+let salida = '';
+for ( let i = 1; i<= 10; i++){
+  salida += `${base} x ${i} =  ${base*i}\n`;
+}
+
+console.log(salida);
+
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err)=>{
+    if(err) throw err;
+    console.log(`Tabla del ${base} creada`);
+});
